e721ecbd2c7e08b185a8bbd39cac4f573681e999
BuildTools <unconfigured@null.spigotmc.org>
2023-11-06 22:48:07 -0500

    Changes

--- src/main/java/io/github/pulsebeat02/earthsmp/mixin/SuspiciousBlockMixin.java
+++ src/main/java/io/github/pulsebeat02/earthsmp/mixin/SuspiciousBlockMixin.java
@@ -1,6 +1,5 @@
 package io.github.pulsebeat02.earthsmp.mixin;
 
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.SplittableRandom;
@@ -12,48 +11,106 @@ import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.entity.BrushableBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
-import net.minecraft.loot.LootTable;
-import net.minecraft.loot.context.LootContextParameterSet;
-import net.minecraft.loot.context.LootContextParameters;
-import net.minecraft.loot.context.LootContextTypes;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.tag.InstrumentTags;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.IndexedIterable;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(BrushableBlockEntity.class)
 public abstract class SuspiciousBlockMixin extends BlockEntity {
 
-  private static final Identifier GRAVEL;
-  private static final List<ItemStack> GOAT_HORNS;
+  private static final List<ItemStack> SAND_DROPS;
+  private static final List<ItemStack> GRAVEL_DROPS;
   private static final SplittableRandom RANDOM;
 
   static {
-    GRAVEL = new Identifier("earthsmp", "archaeology/gravel");
+    SAND_DROPS = new ArrayList<>();
+    GRAVEL_DROPS = new ArrayList<>();
+    registerSandDrops();
+    registerGravelDrops();
+    registerCommonDrops();
+    RANDOM = new SplittableRandom();
+  }
+
+  @Unique
+  private static void registerGravelDrops() {
+    final Item[] drops =
+        new Item[] {
+          Items.ANGLER_POTTERY_SHERD,
+          Items.ARCHER_POTTERY_SHERD,
+          Items.ARMS_UP_POTTERY_SHERD,
+          Items.BLADE_POTTERY_SHERD,
+          Items.BREWER_POTTERY_SHERD,
+          Items.BURN_POTTERY_SHERD,
+          Items.DANGER_POTTERY_SHERD,
+          Items.EXPLORER_POTTERY_SHERD,
+          Items.FRIEND_POTTERY_SHERD,
+          Items.HEART_POTTERY_SHERD,
+          Items.HEARTBREAK_POTTERY_SHERD,
+          Items.HOWL_POTTERY_SHERD,
+          Items.MINER_POTTERY_SHERD,
+          Items.MOURNER_POTTERY_SHERD,
+          Items.PLENTY_POTTERY_SHERD,
+          Items.PRIZE_POTTERY_SHERD,
+          Items.SHEAF_POTTERY_SHERD,
+          Items.SHELTER_POTTERY_SHERD,
+          Items.SKULL_POTTERY_SHERD,
+          Items.SNORT_POTTERY_SHERD,
+        };
+    for (final Item drop : drops) {
+      final ItemStack stack = new ItemStack(drop);
+      GRAVEL_DROPS.add(stack);
+    }
+  }
+
+  @Unique
+  private static void registerSandDrops() {
     final IndexedIterable<RegistryEntry<Instrument>> instruments =
         Registries.INSTRUMENT.getIndexedEntries();
-    GOAT_HORNS = new ArrayList<>();
     while (instruments.iterator().hasNext()) {
       final GoatHornItem item =
           new GoatHornItem(new Item.Settings().maxCount(1), InstrumentTags.GOAT_HORNS);
       final RegistryEntry<Instrument> entry = instruments.iterator().next();
       final ItemStack stack = GoatHornItem.getStackForInstrument(item, entry);
-      GOAT_HORNS.add(stack);
+      SAND_DROPS.add(stack);
     }
-    RANDOM = new SplittableRandom();
+  }
+
+  @Unique
+  private static void registerCommonDrops() {
+    final Item[] drops =
+        new Item[] {
+          Items.AIR,
+          Items.BONE,
+          Items.BONE_MEAL,
+          Items.FLINT,
+          Items.GOLD_NUGGET,
+          Items.GOLD_INGOT,
+          Items.IRON_NUGGET,
+          Items.COBBLESTONE,
+          Items.DEEPSLATE,
+          Items.WHEAT_SEEDS,
+          Items.BEETROOT_SEEDS,
+          Items.MELON_SEEDS,
+          Items.PUMPKIN_SEEDS,
+          Items.TORCHFLOWER_SEEDS
+        };
+    for (final Item drop : drops) {
+      final ItemStack stack = new ItemStack(drop);
+      SAND_DROPS.add(stack);
+      GRAVEL_DROPS.add(stack);
+    }
   }
 
   @Shadow private ItemStack item;
-  @Shadow private long lootTableSeed;
   @Shadow private Identifier lootTable;
 
   public SuspiciousBlockMixin(
@@ -77,23 +134,11 @@ public abstract class SuspiciousBlockMixin extends BlockEntity {
     }
     final BlockState state = this.getCachedState();
     if (state.isOf(Blocks.SUSPICIOUS_GRAVEL)) {
-      final LootTable table = this.world.getServer().getLootManager().getLootTable(GRAVEL);
-      final LootContextParameterSet lootContextParameterSet =
-          new LootContextParameterSet.Builder((ServerWorld) this.world)
-              .add(LootContextParameters.ORIGIN, Vec3d.ofCenter(this.pos))
-              .luck(player.getLuck())
-              .add(LootContextParameters.THIS_ENTITY, player)
-              .build(LootContextTypes.CHEST);
-      final ObjectArrayList<ItemStack> objectArrayList =
-          table.generateLoot(lootContextParameterSet, this.lootTableSeed);
-      this.item =
-          switch (objectArrayList.size()) {
-            case 0 -> ItemStack.EMPTY;
-            default -> objectArrayList.get(0);
-          };
+      final int index = RANDOM.nextInt(GRAVEL_DROPS.size());
+      this.item = GRAVEL_DROPS.get(index);
     } else if (state.isOf(Blocks.SUSPICIOUS_SAND)) {
-      final int index = RANDOM.nextInt(GOAT_HORNS.size());
-      this.item = GOAT_HORNS.get(index);
+      final int index = RANDOM.nextInt(SAND_DROPS.size());
+      this.item = SAND_DROPS.get(index);
     }
     this.markDirty();
   }
