e9d78d965fbad251c0e7374c72ce547ecfc1bca8
BuildTools <unconfigured@null.spigotmc.org>
2023-11-06 19:18:37 -0500

    Changes

--- src/main/java/io/github/pulsebeat02/earthsmp/mixin/VelocityNerfMixin.java
+++ src/main/java/io/github/pulsebeat02/earthsmp/mixin/VelocityNerfMixin.java
@@ -1,33 +1,23 @@
 package io.github.pulsebeat02.earthsmp.mixin;
 
-import io.github.pulsebeat02.earthsmp.utils.Utils;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class VelocityNerfMixin {
 
-  @Inject(method = "tickFallFlying", at = @At("TAIL"))
-  private void tickFallFlying(@NotNull final CallbackInfo ci) {
-    final LivingEntity entity = (LivingEntity) (Object) this;
-    final double speedLimit = 0.5D;
-    final Vec3d velocity = entity.getVelocity();
-    final double speed = new Vec3d(velocity.getX(), 0.0f, velocity.getZ()).lengthSquared();
-    final double hScale = Math.sqrt(speedLimit / speed);
-    if (hScale >= 0.0D && hScale <= 100.0D) {
-      final double vScale = 1.0D;
-      final Vec3d reducedSpeed =
-          Utils.cloneVector(velocity).multiply(new Vec3d(hScale, vScale, hScale));
-      entity.setVelocity(reducedSpeed);
-    }
+  @ModifyArg(
+      method = "Lnet/minecraft/entity/LivingEntity;travel(Lnet/minecraft/util/math/Vec3d;)V",
+      at =
+          @At(
+              value = "INVOKE",
+              target =
+                  "Lnet/minecraft/entity/LivingEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V",
+              ordinal = 6))
+  private @NotNull Vec3d modifyVelocity(@NotNull final Vec3d vec3) {
+    return vec3.multiply(10E-5);
   }
 }
