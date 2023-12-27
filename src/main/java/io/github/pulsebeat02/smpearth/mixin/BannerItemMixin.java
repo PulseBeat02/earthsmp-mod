f211201011b4cd8c5a972544f1b9f763057e8f07
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2023-12-27 13:45:12 -0500

    jjhasf

--- src/main/java/io/github/pulsebeat02/smpearth/mixin/BannerItemMixin.java
+++ src/main/java/io/github/pulsebeat02/smpearth/mixin/BannerItemMixin.java
@@ -1,16 +1,52 @@
 package io.github.pulsebeat02.smpearth.mixin;
 
+import java.util.List;
+import net.minecraft.block.entity.BannerPattern;
 import net.minecraft.item.BannerItem;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.text.Text;
+import net.minecraft.util.DyeColor;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(BannerItem.class)
 public final class BannerItemMixin {
-  @ModifyConstant(
-      method = "appendBannerTooltip(Lnet/minecraft/item/ItemStack;Ljava/util/List;)V",
-      constant = @Constant(intValue = 6))
-  private static int maxBannerPattern(final int orig) {
-    return Integer.MAX_VALUE;
+
+  /**
+   * @author PulseBeat_02
+   * @reason removes banner limit
+   */
+  @Overwrite
+  public static void appendBannerTooltip(final ItemStack stack, final List<Text> tooltip) {
+    final NbtCompound nbtCompound = BlockItem.getBlockEntityNbt(stack);
+    if (nbtCompound != null && nbtCompound.contains("PatternList")) {
+      final NbtList nbtList = nbtCompound.getList("PatternList", 10);
+      for (int i = 0; i < nbtList.size(); ++i) {
+        final NbtCompound nbtCompound2 = nbtList.getCompound(i);
+        final DyeColor dyeColor = DyeColor.byId(nbtCompound2.getInt("Color"));
+        final RegistryEntry<BannerPattern> registryEntry =
+            BannerPattern.byId(nbtCompound2.getString("Pattern"));
+        if (registryEntry != null) {
+          registryEntry
+              .getKey()
+              .map((key) -> key.getValue().toShortTranslationKey())
+              .ifPresent(
+                  (translationKey) -> {
+                    tooltip.add(
+                        Text.translatable(
+                                "block.minecraft.banner."
+                                    + translationKey
+                                    + "."
+                                    + dyeColor.getName())
+                            .formatted(Formatting.GRAY));
+                  });
+        }
+      }
+    }
   }
 }
