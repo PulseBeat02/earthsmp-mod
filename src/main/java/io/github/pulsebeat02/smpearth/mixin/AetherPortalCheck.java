66296a719f36c637544e769192955cafa358049b
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2024-01-01 17:32:52 -0500

    changes

--- src/main/java/io/github/pulsebeat02/smpearth/mixin/AetherPortalCheck.java
+++ src/main/java/io/github/pulsebeat02/smpearth/mixin/AetherPortalCheck.java
@@ -1,20 +1,30 @@
 package io.github.pulsebeat02.smpearth.mixin;
 
+import java.util.concurrent.CompletableFuture;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.PlainTextContent;
+import net.minecraft.text.Style;
+import net.minecraft.text.TextContent;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(BlockItem.class)
+@Mixin(BucketItem.class)
 public final class AetherPortalCheck {
 
   //
@@ -25,54 +35,56 @@ public final class AetherPortalCheck {
   // X X X X
   //
 
-  @Unique
-  private static final boolean[][] PORTAL_SHAPE;
+  @Shadow @Final private Fluid fluid;
+  @Unique private static final boolean[][] PORTAL_SHAPE;
 
   static {
     PORTAL_SHAPE =
         new boolean[][] {
-          new boolean[] {true, true, true, true, true},
-          new boolean[] {true, false, false, false, true},
-          new boolean[] {true, false, false, false, true},
-          new boolean[] {true, false, false, false, true},
-          new boolean[] {true, false, false, false, true},
-          new boolean[] {true, true, true, true, true},
+          new boolean[] {true, true, true, true},
+          new boolean[] {true, false, false, true},
+          new boolean[] {true, false, false, true},
+          new boolean[] {true, false, false, true},
+          new boolean[] {true, true, true, true},
         };
   }
 
-  @Inject(
-      method = "place(Lnet/minecraft/item/ItemPlacementContext;Lnet/minecraft/block/BlockState;)Z",
-      at = @At("HEAD"))
-  private void onPlace(
-      @NotNull final ItemPlacementContext context,
-      @NotNull final BlockState state,
-      @NotNull final CallbackInfoReturnable<Boolean> cir) {
+  @Inject(method = "onEmptied", at = @At("HEAD"))
+  private void onBucketEmpty(
+      @NotNull final PlayerEntity player,
+      @NotNull final World world,
+      @NotNull final ItemStack stack,
+      @NotNull final BlockPos pos,
+      @NotNull final CallbackInfo ci) {
 
-    final PlayerEntity entity = context.getPlayer();
-    final BlockPos pos = context.getBlockPos();
-    if (!this.checkWater(state)) {
+    if (!this.checkWater()) {
       return;
     }
 
-    final BlockPos topLeft = this.getTopLeft(entity, pos);
-    if (!this.checkPortal(entity, topLeft)) {
+    final BlockPos topLeft = this.getTopLeft(player, pos);
+    if (!this.checkPortal(player, topLeft)) {
       return;
     }
 
-    this.lightPortal(entity, topLeft);
+    this.sendMessage(player);
   }
 
   @Unique
-  private void lightPortal(@NotNull final PlayerEntity entity, @NotNull final BlockPos topLeft) {
-    final World world = entity.getWorld();
-    for (int x = 0; x < PORTAL_SHAPE.length; x++) {
-      for (int z = 0; z < PORTAL_SHAPE[x].length; z++) {
-        final BlockPos pos = topLeft.add(x, z, 0);
-        if (!PORTAL_SHAPE[x][z]) {
-          world.setBlockState(pos, Blocks.NETHER_PORTAL.getDefaultState());
-        }
-      }
-    }
+  private void sendMessage(@NotNull final PlayerEntity entity) {
+    CompletableFuture.runAsync(
+        () -> {
+          entity.sendMessage(this.createLiteral("Did you just try to create an Aether portal?"));
+          entity.sendMessage(
+              this.createLiteral(
+                  "Add the two pairs of coordinates you have gotten together, and you will find the end portal then."));
+        });
+  }
+
+  @Unique
+  private @NotNull MutableText createLiteral(@NotNull final String message) {
+    final TextContent literal = new PlainTextContent.Literal(message);
+    final Style style = Style.EMPTY.withColor(Formatting.GOLD).withItalic(false);
+    return MutableText.of(literal).setStyle(style);
   }
 
   @Unique
@@ -91,8 +103,8 @@ public final class AetherPortalCheck {
   }
 
   @Unique
-  private boolean checkWater(@NotNull final BlockState state) {
-    return state.isOf(Blocks.WATER);
+  private boolean checkWater() {
+    return this.fluid.matchesType(Fluids.WATER);
   }
 
   @Unique
