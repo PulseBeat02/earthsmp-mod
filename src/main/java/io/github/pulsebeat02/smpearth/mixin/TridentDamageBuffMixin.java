b1c720107c6b7e1ea8237896f440922619fbc9b4
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2024-01-04 16:40:40 -0500

    Added Trident Buffs

---
+++ src/main/java/io/github/pulsebeat02/smpearth/mixin/TridentDamageBuffMixin.java
@@ -0,0 +1,22 @@
+package io.github.pulsebeat02.smpearth.mixin;
+
+import net.minecraft.entity.projectile.TridentEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(TridentEntity.class)
+public final class TridentDamageBuffMixin {
+
+  @ModifyArg(
+      method = "onEntityHit",
+      at =
+          @At(
+              value = "INVOKE",
+              target =
+                  "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"),
+      index = 1)
+  private float changeDamage(final float g) {
+    return g * 1.5f;
+  }
+}
